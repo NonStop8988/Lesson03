51f3f9f1a3cb6a73cd41bbe8308836db5dbefc09
Maxim <lyskomax2@mail.ru>
2024-01-13 18:24:13 +0300

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿void ZeroEvenElemets(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+ZeroEvenElemets(array);
+PrintArray(array);
