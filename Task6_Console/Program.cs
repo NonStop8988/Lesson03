33f31f0f97be0f24eaaac5992da967ad035b00c4
Maxim <lyskomax2@mail.ru>
2024-01-13 20:18:09 +0300

    Add Task6_Console

---
+++ Task6_Console/Program.cs
@@ -0,0 +1,11 @@
+﻿int size = 10;
+int[] arr_int = new int [size];
+int i = 0;
+
+while (i < size)
+{
+    Console.WriteLine("Введите элемент массива (целое число):");
+    string input = Console.ReadLine();
+    arr_int[i] = Convert.ToInt32(input);
+    i++;
+}
