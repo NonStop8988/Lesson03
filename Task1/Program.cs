4519bdc85faa687577fac05723e362f8a685c242
Maxim <lyskomax2@mail.ru>
2024-01-13 17:13:38 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿double CalculateFormula(int a, int b, int c, int d) // Создаем функцию
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4); // Запускаем созданную нами функцию
+Console.Write(result);
+
