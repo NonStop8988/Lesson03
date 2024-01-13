f514fe5eee456831e7770f2628581a88b8a622b2
Maxim <lyskomax2@mail.ru>
2024-01-13 17:52:35 +0300

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintSquares (int limit)
+{
+    int i = 0;
+    while (i <= limit)
+    {
+        Console.Write($"{i*i} ");
+        i++;
+    }
+}
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
