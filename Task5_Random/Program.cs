633e7f19d04987c4a654d235dd903a7b9eff4437
Maxim <lyskomax2@mail.ru>
2024-01-13 19:49:24 +0300

    Add Task5_Random

---
+++ Task5_Random/Program.cs
@@ -0,0 +1,12 @@
+﻿Random rnd = new Random();
+
+int size = 10;
+int[] arr_int = new int [size];
+int i = 0;
+
+while (i < size)
+{
+    arr_int[i] = rnd.Next(1, 10); // Верхняя граница (10 в данном случае) не включается в генерируемые случайные числа
+    Console.Write($"{arr_int[i]} ");
+    i++;
+}
